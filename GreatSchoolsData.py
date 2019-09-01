c54ca7bd3e88a6c781bc8d9d5931528793c49d3a
rishsol <rishabsolanki09@gmail.com>
2019-09-01 10:43:54 -0400

    Scraping table of NJ townships

--- GreatSchoolsData.py
+++ GreatSchoolsData.py
@@ -1,1 +1,23 @@
-import pandas
+import pandas as pd
+from bs4 import BeautifulSoup
+import requests
+
+url = 'https://en.wikipedia.org/wiki/List_of_municipalities_in_New_Jersey'
+parsed_url = requests.get(url)
+
+soup = BeautifulSoup(parsed_url.content, 'html.parser')
+soup.prettify()
+
+table = soup.find('table', {'class': 'wikitable sortable'})
+#links = table.findAll('a')
+print(table.text)
+
+nj_cities = []
+nj_cities_df = pd.DataFrame()
+#for link in links:
+ #  nj_cities.append(link.get('title'))
+
+
+#nj_cities_df['NJ Cities'] = nj_cities
+
+#print(nj_cities_df)
