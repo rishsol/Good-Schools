6e765ef24f83245b4a777cdb5b2a96fe06bebc78
rishsol <rishabsolanki09@gmail.com>
2019-09-02 13:46:20 -0400

    Used pandas read_html function to obtain list of NJ townships

--- GreatSchoolsData.py
+++ GreatSchoolsData.py
@@ -1,23 +1,14 @@
 import pandas as pd
 from bs4 import BeautifulSoup
 import requests
+from pandas.io.html import read_html
 
 url = 'https://en.wikipedia.org/wiki/List_of_municipalities_in_New_Jersey'
-parsed_url = requests.get(url)
 
-soup = BeautifulSoup(parsed_url.content, 'html.parser')
-soup.prettify()
+data = pd.read_html(url, attrs= {'class': 'wikitable sortable'})
 
-table = soup.find('table', {'class': 'wikitable sortable'})
-#links = table.findAll('a')
-print(table.text)
+nj_cities_table = data[0]
+nj_cities = nj_cities_table['Municipality']
 
-nj_cities = []
-nj_cities_df = pd.DataFrame()
-#for link in links:
- #  nj_cities.append(link.get('title'))
+print(nj_cities)
 
-
-#nj_cities_df['NJ Cities'] = nj_cities
-
-#print(nj_cities_df)
