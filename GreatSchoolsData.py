2c91e3de89a03b2458a523041f3284a32eb33b88
rishsol <rishabsolanki09@gmail.com>
2019-09-16 11:55:50 -0400

    Will likely not use pandas for this

--- GreatSchoolsData.py
+++ GreatSchoolsData.py
@@ -13,21 +13,20 @@ from pandas.io.html import read_html
 
 path = '/Users/rishabsolanki09@gmail.com/Downloads/Sale_Prices_City.csv'
 all_cities = pd.read_csv(path)
-
 nj_data = pd.DataFrame()
 nj_cities = pd.Series()
 medianPriceList = pd.Series()
 
 i = 0
-while i < 10:
+while i < len(all_cities['RegionID']):
     if all_cities['StateName'][i] == 'New Jersey':
-        nj_cities.append([all_cities['RegionName'][i]])
+        nj_cities.append(([all_cities['RegionName'][i]]))
         medianPriceList.append([all_cities['2019-07'][i]])
 
     i += 1
 
 #nj_cities.insert(0, 'Median Home Price (07-2019)', medianPriceList, True)
-nj_data.append(nj_cities, ignore_index= True)
+#nj_data.append(nj_cities, ignore_index= True)
 
 print(nj_cities)
 #print(all_cities)
